502a3acbddc5025a2211f910aa2c20310b7f80c6
AnthonyPec <38145775+AnthonyPec@users.noreply.github.com>
2020-09-16 22:05:35 -0400

    Added fuzzy_c_means.py and changed clustering to use plotly

--- fuzzy_c_means.py
+++ fuzzy_c_means.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
 
+m = 4
 # cols to use for clustering
 cols = ['NoFaceContact', 'Risk', 'Sick']
 
@@ -29,25 +30,38 @@ def dist(list1, list2):
         val += (list1[i] - list2[i]) ** 2
 
 
-def k_means(df, cluster_cen):
+def c_means(df, cluster_cen):
+    tmp_list = []
     for i in range(0, k):
         rand = 0
-        col_name = 'k cluster ' + str(i)
+        col_name = 'mem' + str(i)
 
         for n in range(0, len(cols)):
             rand += rand + (df[cols[n]] - cluster_cen[i][n]) ** 2
+        tmp_list.append(rand)
+    #mem = 1 / ((tmp_list[0] / ((tmp_list[1]) ** (2 / (m - 1)))) + (tmp_list[0] / ((tmp_list[2]) ** (2 / (m - 1)))))
 
-        df[col_name] = rand
+    for p in range(0, k):
+        col_name = 'mem' + str(p)
+        dem = 0
+        for q in range(0, k):
+            dem = dem + (tmp_list[p] / (tmp_list[q])) ** (2 / (m - 1))
 
-
-def classify_k_means(df):
-    df['cluster_mem'] = df[cluster_cols].idxmin(axis=1)
+        df[col_name] = 1 / dem
 
 
 def recenter_clusters(df):
-    cluster = df.groupby('cluster_mem').mean()
-    cluster_cen = cluster[cols].values.tolist()
-    return cluster_cen
+    #cluster = df.groupby('cluster_mem').mean()
+    #cluster_cen = cluster[cols].values.tolist()
+    points = df[cols].values.tolist()
+    for p in range(0,k):
+
+        name = 'mem'+str(p)
+        temp = df[cols].multiply(df[name],axis = 'index')
+        print("")
+       # for q in points:
+        #temp = df['mem'+str(p)].values.tolist()
+    #return cluster_cen
 
 
 def read_data(file):
@@ -59,34 +73,10 @@ def read_data(file):
     return df
 
 
-def k_means_chart(df):
-    colmap = {'k cluster 0': 'r', 'k cluster 1': 'g', 'k cluster 2': 'b', 'k cluster 3': 'purple'}
-    # colors = ['blue', 'green', 'red']
-    fig = plt.figure(figsize=(16, 9))
-    ax = plt.axes(projection="3d")
-
-    labels = list(set(df['cluster_mem'].tolist()))
-    centroids = cluster_list
-    # colors = map(lambda x: colmap[x], labels)
-
-    # ax.scatter3D(df['NoFaceContact'], df['Risk'], df['Sick'], color=list(colors), alpha=0.5)
-
-    for i in range(len(df)):
-        ax.scatter3D(df.iloc[i]['NoFaceContact'], df.iloc[i]['Risk'], df.iloc[i]['Sick'],
-                     c=colmap[(df.iloc[i]['cluster_mem'])])
-
-    plt.show()
-
-
 def plotly_chart(df):
     fig = px.scatter_3d(df, x='NoFaceContact', y='Risk', z='Sick',
                         color='cluster_mem')
     fig.show()
-
-
-#def dunns_index(df):
-
-
 
 
 # sample = {'X': [1, 2, 4, 7], 'Y': [3, 5, 8, 9]}
@@ -103,15 +93,14 @@ data_frame = read_data(file)
 
 cluster_cols = []
 for i in range(0, k):
-    cluster_cols.append('k cluster ' + str(i))
+    cluster_cols.append('mem' + str(i))
 
 cluster_list = find_starting_clusters(data_frame, k).values.tolist()
 print(cluster_list)
 while con:
     i += 1
     old_set = cluster_list
-    k_means(data_frame, cluster_list)
-    classify_k_means(data_frame)
+    c_means(data_frame, cluster_list)
     cluster_list = recenter_clusters(data_frame)
     new_set = cluster_list
     print(data_frame)
@@ -119,7 +108,7 @@ while con:
         print(i)
         con = False
 
-#k_means_chart(data_frame)
+# k_means_chart(data_frame)
 plotly_chart(data_frame)
 # print(cluster_list)
 # print(update_membership(df,2,cluster_list))
