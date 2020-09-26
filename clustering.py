fixed fuzzy-c means, used common naming convention for cols in fuzzy and k means clustering. Added method to get average dunn index after 50 iterations

clustering.py
@@ -1,18 +1,11 @@
 import pandas as pd
-import math
 import numpy as np
-import matplotlib.pyplot as plt
 import plotly.express as px
 import dunns_index as di
 
-
-class NotConvergeError(Exception):
-    """K means algo failed to converge"""
-    pass
-
-
 # cols to use for clustering
-cols = ['NoFaceContact', 'Risk', 'Sick','HandSanit']
+cols = ['NoFaceContact', 'Risk', 'Sick']
+
 
 
 def normalize(df):
@@ -23,20 +16,20 @@ def normalize(df):
         result[feature_name] = (df[feature_name] - min_value) / (max_value - min_value)
     return result
 
+def standardize_data(df):
+    result = df.copy()
+    for feature_name in df.columns:
+        # Risk is already normalized
+        if feature_name == 'Risk':
+            continue
+        result[feature_name] = (df[feature_name] - df[feature_name].mean()) / df[feature_name].std()
+    return result
+
 
 def find_starting_clusters(df, k):
     sample_df = df.drop_duplicates()
     starting_pts = sample_df.sample(n=k)
     return starting_pts
-
-
-def update_membership(df, k, cluster_df):
-    for i in range(0, k):
-        col_name = 'cluster ' + str(i)
-
-        df[col_name] = 1.0 / (df['X'] - cluster_df[i][0])
-
-    return df
 
 
 def dist(list1, list2):
@@ -46,9 +39,12 @@ def dist(list1, list2):
 
 
 def calc_dist(df, cluster_cen, k):
+    '''
+    calc distance from each point to each cluster center
+    '''
     for i in range(0, k):
         rand = 0
-        col_name = 'k cluster ' + str(i)
+        col_name = 'cluster ' + str(i)
 
         for n in range(0, len(cols)):
             try:
@@ -56,59 +52,60 @@ def calc_dist(df, cluster_cen, k):
             except:
                 print('here')
 
-        df[col_name] = rand
+        df[col_name] = rand ** (1 / 2)
 
 
-def classify_k_means(df, cluster_cols,k):
+def classify_k_means(df, cluster_cols, k):
+    '''
+    classify each row of dataframe by assigning it to cluster closest to it
+    '''
     df['cluster_mem'] = df[cluster_cols].idxmin(axis=1)
 
     count = df['cluster_mem'].unique()
 
-    # cluster was lost, k means failed to converge
+    # some clusters are empty need to replace with random point
     if len(count) < int(k):
-        raise NotConvergeError
+        empty_sets = set(cluster_cols) - set(count)
+        sample_df = df.drop_duplicates()
+        starting_pts = sample_df.sample(n=len(empty_sets))
+        test = starting_pts.index.values
+        for i in range(0, len(empty_sets)):
+            df.loc[test[i], 'cluster_mem'] = empty_sets.pop()
 
-def recenter_clusters(df,k):
+
+def recenter_clusters(df, k):
+    '''
+    recalc cluster centers based on what points are in cluster
+    '''
     cluster = df.groupby('cluster_mem').mean()
     cluster_cen = cluster[cols].values.tolist()
     return cluster_cen
 
 
 def read_data(file):
+    '''
+    load csv into pandas, and drop null values and noise
+    '''
     df = pd.read_csv(file)
 
     drop_col = (df.columns[~df.columns.isin(cols)].tolist())
+    df = df.loc[df['NoFaceContact'] < 9]
     df.drop(drop_col, axis=1, inplace=True)
     df.dropna(0, inplace=True)
     return df
 
 
-def k_means_chart(df):
-    colmap = {'k cluster 0': 'r', 'k cluster 1': 'g', 'k cluster 2': 'b', 'k cluster 3': 'purple'}
-    # colors = ['blue', 'green', 'red']
-    fig = plt.figure(figsize=(16, 9))
-    ax = plt.axes(projection="3d")
-
-    labels = list(set(df['cluster_mem'].tolist()))
-    # centroids = cluster_list
-    # colors = map(lambda x: colmap[x], labels)
-
-    # ax.scatter3D(df['NoFaceContact'], df['Risk'], df['Sick'], color=list(colors), alpha=0.5)
-
-    for i in range(len(df)):
-        ax.scatter3D(df.iloc[i]['NoFaceContact'], df.iloc[i]['Risk'], df.iloc[i]['Sick'],
-                     c=colmap[(df.iloc[i]['cluster_mem'])])
-
-    plt.show()
-
 
 def plotly_chart(df):
+    '''
+    plot results
+    '''
     fig = px.scatter_3d(df, x='NoFaceContact', y='Risk', z='Sick',
                         color='cluster_mem', symbol='type')
     fig.show()
 
 
-def k_means(k):
+def k_means(k, plot = True):
     k = k
 
     con = True
@@ -118,22 +115,25 @@ def k_means(k):
     new_set = set()
     file = r"C:\Users\Anthony\Downloads\Assignment1_Data.csv"
     data_frame = read_data(file)
-    data_frame = normalize(data_frame)
+
+    data_frame = standardize_data(data_frame)
+    #data_frame = normalize(data_frame)
+
+
     cluster_cols = []
     for i in range(0, k):
-        cluster_cols.append('k cluster ' + str(i))
+        cluster_cols.append('cluster ' + str(i))
 
     cluster_list = find_starting_clusters(data_frame, k).values.tolist()
-
     while con:
         i += 1
         old_set = cluster_list
         calc_dist(data_frame, cluster_list, k)
-        classify_k_means(data_frame, cluster_cols,k)
-        cluster_list = recenter_clusters(data_frame,k)
+        classify_k_means(data_frame, cluster_cols, k)
+        cluster_list = recenter_clusters(data_frame, k)
         new_set = cluster_list
         if i > max_iter or old_set == new_set:
-            #print(i)
+            # print(i)
             con = False
 
     zeroes = []
@@ -148,24 +148,35 @@ def k_means(k):
     temp = pd.DataFrame(cluster_list, columns=list(data_frame.columns))
     data_frame = data_frame.append(temp)
     #plotly_chart(data_frame)
-    return data_frame
+    #print(di.calc_dunn_index(data_frame, k, cols))
+    return data_frame, cluster_list
 
 
-
-def avg_dunn_index():
+def run_multi_dunn_index(start_clust, end_clust):
+    #run multi iterations of k means and calc avg DI and highest DI
     avg_dunn_index = np.zeros(11)
-    for index in range(0, 5):
-        for k in range(2, 11):
-            data_frame = k_means(k)
+    high_dunn_index = np.zeros(11)
+    for index in range(0, 50):
+        for k in range(start_clust, end_clust+1):
+            data_frame, clust_list = k_means(k,False)
             dunn_index = di.calc_dunn_index(data_frame, k, cols)
             avg_dunn_index[k] += dunn_index
-    avg_dunn_index = avg_dunn_index / 20
-    for i in range(2,11):
-        print('K = ' + str(i) + ' has a dunn index of ' + str(avg_dunn_index[i]))
+            if dunn_index > high_dunn_index[k]:
+                high_dunn_index[k] = dunn_index
+    avg_dunn_index = avg_dunn_index / 50
+    for i in range(start_clust, end_clust+1):
+        print('K = ' + str(i) + ' has an average dunn index of ' + str(avg_dunn_index[i]))
+    for i in range(start_clust,end_clust+1):
+        print('K = ' + str(i) + ' highest dunn index of ' + str(high_dunn_index[i]))
 
 
-avg_dunn_index()
-#for k in range(2, 10):
-#    data_frame = k_means(k)
-# for cluster_num in range(2, 10):
-# k_means(cluster_num)
+#run dunn index for k = 2 to 10
+
+# run k means for k =2 to k =10 and print cluster points and plot each
+for k in range(2, 10):
+    data_frame,clust_list = k_means(k)
+    for i in clust_list:
+        print(i[0:3])
+
+# run DI on k = 2 to 10
+run_multi_dunn_index(2,10)

fuzzy_c_means.py
@@ -3,48 +3,49 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
+import dunns_index as di
 
+# parameter to control how fuzzy cluster will be
 m = 2
+
 # cols to use for clustering
 cols = ['NoFaceContact', 'Risk', 'Sick']
 
 
+
 def find_starting_clusters(df, k):
+    '''
+    find starting clusters for fuzzy c-means
+    '''
     sample_df = df.drop_duplicates()
-    starting_pts = sample_df.sample(n=k, random_state=1)
+    starting_pts = sample_df.sample(n=k)
+
     starting_pts = starting_pts + 0.01
+
     return starting_pts
 
 
-def normalize(df):
+def standardize_data(df):
+    '''
+    standardize each column besides Risk since it is already standardize
+    '''
     result = df.copy()
     for feature_name in df.columns:
-        max_value = df[feature_name].max()
-        min_value = df[feature_name].min()
-        result[feature_name] = (df[feature_name] - min_value) / (max_value - min_value)
+        # Risk is already normalized
+        if feature_name == 'Risk':
+            continue
+        result[feature_name] = (df[feature_name] - df[feature_name].mean()) / df[feature_name].std()
     return result
 
 
-def update_membership(df, k, cluster_df):
-    for i in range(0, k):
-        col_name = 'cluster ' + str(i)
-
-        df[col_name] = 1.0 / (df['X'] - cluster_df[i][0])
-
-    return df
-
-
-def dist(list1, list2):
-    val = 0
-    for i in list1:
-        val += (list1[i] - list2[i]) ** 2
-
-
 def calc_mem(df, cluster_cen, k):
+    '''
+    calculate membership of each entry in dataframe for each cluster
+    '''
     tmp_list = []
     for i in range(0, k):
         rand = 0
-        col_name = 'mem' + str(i)
+        col_name = 'cluster ' + str(i)
 
         for n in range(0, len(cols)):
             rand += rand + (df[cols[n]] - cluster_cen[i][n]) ** 2
@@ -52,7 +53,7 @@ def calc_mem(df, cluster_cen, k):
     # mem = 1 / ((tmp_list[0] / ((tmp_list[1]) ** (2 / (m - 1)))) + (tmp_list[0] / ((tmp_list[2]) ** (2 / (m - 1)))))
 
     for p in range(0, k):
-        col_name = 'mem' + str(p)
+        col_name = 'cluster ' + str(p)
         dem = 0
         for q in range(0, k):
             dem = dem + (tmp_list[p] / (tmp_list[q])) ** (2 / (m - 1))
@@ -61,14 +62,18 @@ def calc_mem(df, cluster_cen, k):
 
 
 def recenter_clusters(df, k):
+    '''
+    calculate new cluster centers
+    '''
     # cluster = df.groupby('cluster_mem').mean()
     cluster_cen = []
     points = df[cols].values.tolist()
     for p in range(0, k):
-        name = 'mem' + str(p)
-        temp = df[cols].multiply(df[name], axis='index')
+        name = 'cluster ' + str(p)
+
+        temp = df[cols].multiply(df[name] ** (2), axis='index')
         test = temp.sum()
-        test2 = df[name].sum().tolist()
+        test2 = (df[name] ** 2).sum().tolist()
         cluster = (test / test2).tolist()
         cluster_cen.append(cluster)
     return cluster_cen
@@ -78,63 +83,129 @@ def recenter_clusters(df, k):
 
 
 def read_data(file):
+    '''
+    load csv into pandas, and drop null values and noise
+    '''
     df = pd.read_csv(file)
-
     drop_col = (df.columns[~df.columns.isin(cols)].tolist())
     df.drop(drop_col, axis=1, inplace=True)
     df.fillna(0, inplace=True)
+    df = df.loc[df['NoFaceContact'] < 9]
     # df = pd.DataFrame([[0,1,2,],[1,2,1],[0,0,0]], columns =['NoFaceContact', 'Risk','Sick'])
     return df
 
 
 def plotly_chart(df):
+    '''
+    plot results
+    '''
     fig = px.scatter_3d(df, x='NoFaceContact', y='Risk', z='Sick',
-                        color='mem0', symbol='type')
+                        color='cluster 0', symbol='type')
 
     fig.show()
 
 
+def harden_cluster(df, cluster_cols):
+    '''
+    assign cluster to each row based on highest memebership
+    '''
+    df['cluster_mem'] = df[cluster_cols].idxmax(axis=1)
+
+
 def fuzzy_c_means(k):
+    '''
+    fuzzy c-means algo, find starting cluster then calc mem , then recalculate centers, repeat this
+    until max iterations are meet or we dont decrease obj function
+    '''
     k = k
 
     con = True
-    max_iter = 5000
+
+    max_iter = 500
     i = 0
     old_set = set()
     new_set = set()
     file = r"C:\Users\Anthony\Downloads\Assignment1_Data.csv"
     data_frame = read_data(file)
-    data_frame = normalize(data_frame)
+    data_frame = standardize_data(data_frame)
     cluster_cols = []
     for i in range(0, k):
-        cluster_cols.append(1)
+        cluster_cols.append('cluster ' + str(i))
 
     cluster_list = find_starting_clusters(data_frame, k).values.tolist()
-    print(cluster_list)
-
+    min_sum = 999999
     while con:
         i += 1
-        old_set = cluster_list
         calc_mem(data_frame, cluster_list, k)
         cluster_list = recenter_clusters(data_frame, k)
-        new_set = cluster_list
+        sum = obj_fun(data_frame, cluster_list, k)
+        if sum < min_sum:
+            min_sum = sum
+        else:
+            print(i)
+            con = False
 
-        if i > max_iter or old_set == new_set:
+        if i > max_iter:
             print(i)
             con = False
 
     zeroes = []
-    zeroes.extend(cluster_cols)
-    zeroes.append('Center')
+    for clust in cluster_cols:
+        zeroes.append(.5)
+    zeroes.extend(['Center', 'Cluster'])
     for clust in cluster_list:
         clust.extend(zeroes)
     # k_means_chart(data_frame)
     data_frame['type'] = 'cluster'
-    test = list(data_frame.columns)
+    harden_cluster(data_frame, cluster_cols)
     temp = pd.DataFrame(cluster_list, columns=list(data_frame.columns))
     data_frame = data_frame.append(temp)
-
     plotly_chart(data_frame)
+    return data_frame, cluster_list
 
 
-fuzzy_c_means(2)
+def obj_fun(df, cluster_list, k):
+    '''
+    objective function we are trying to minimize for fuzzy c means
+    '''
+    tmp_list = []
+    sum = 0
+    for i in range(0, k):
+        rand = 0
+        col_name = 'cluster ' + str(i)
+
+        for n in range(0, len(cols)):
+            rand += rand + (df[cols[n]] - cluster_list[i][n]) ** 2
+        rand = rand ** (1 / 2)
+        sum += df[col_name].multiply(rand).sum()
+    return sum
+
+
+def run_multi_dunn_index(start_clust, end_clust):
+    '''
+    run multi iterations of fuzzy c means and calc avg DI and highest DI
+    '''
+    avg_dunn_index = np.zeros(11)
+    high_dunn_index = np.zeros(11)
+    for index in range(0, 50):
+        for k in range(start_clust, end_clust + 1):
+            data_frame, clust_list = fuzzy_c_means(k)
+            dunn_index = di.calc_dunn_index(data_frame, k, cols)
+            avg_dunn_index[k] += dunn_index
+            if dunn_index > high_dunn_index[k]:
+                high_dunn_index[k] = dunn_index
+    avg_dunn_index = avg_dunn_index / 50
+    for i in range(start_clust, end_clust + 1):
+        print('K = ' + str(i) + ' has an average dunn index of ' + str(avg_dunn_index[i]))
+    for i in range(start_clust, end_clust + 1):
+        print('K = ' + str(i) + ' highest dunn index of ' + str(high_dunn_index[i]))
+
+
+
+# run fuzzy c-means with k = 3 and print centers
+data_frame, clust_list = fuzzy_c_means(3)
+for i in clust_list:
+    print(i[0:3])
+
+# run dunn index on with k = 3
+run_multi_dunn_index(3, 3)